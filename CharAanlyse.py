05e4b8281a74e1918c49d9b41c1fa0a18dd0e6b6
马灰 <912990012@qq.com>
2017-08-21 20:53:44 +0800

    修改 show_dot_pic的传入的字符串参数，输出对应的字符画，然后依照这个进行github的提交，可以用github写小说惹。但是输入中文会有问题。暂时只能支持ascii字符串。┓( ´∀` )┏

--- CharAanlyse.py
+++ CharAanlyse.py
@@ -1,19 +1,66 @@
 # -*-coding:utf-8 -*-
+import re
 
-def show_tb(gittb):
-    '''
-    输入的数组为0,1二维数组， 先不考虑存储吧
-    '''
-    for i in range(len(gittb)):
-        # print gittb[i]
-        for dat in gittb[i]:
-            if dat == 0:
-                print '-',
-            elif dat == 1:
-                print '+',
-        print(' ')
+from PIL import Image
+from PIL import ImageDraw
 
 
-def test():
-    show_tb('a')
-test()
+SINGLE_HEIGHT = 10
+SINGLE_WIDTH = 6
+
+Char_Empty = '-'
+Char_Full = '*'
+Empty_Re = '(------+\n)+'  # 修改了Char_Empty 需要修改Empty_Re,使其可以去除连续的内容的字符
+
+
+# def show_tb(strtbl):
+#     '''
+#     输入的数组为0,1二维数组， 先不考虑存储吧
+#     '''
+#     for i in range(len(strtbl)):
+#         # print gittb[i]
+#         for dat in strtbl[i]:
+#             if dat == 0:
+#                 print '-',
+#             elif dat == 1:
+#                 print '+',
+#         print(' ')
+
+def create_image(str):
+    im = Image.new('1', (SINGLE_WIDTH * len(str), SINGLE_HEIGHT), 'white')
+    draw = ImageDraw.Draw(im)
+    draw.text((0,0),str)
+    return im
+
+def image_to_dot(pic , width=SINGLE_WIDTH, height=SINGLE_HEIGHT):
+    im = pic
+    im = im.resize((width, height), Image.NEAREST)
+
+    ret = []
+    for h in xrange(height):
+        li = []
+        for w in xrange(width):
+            gray  = im.getpixel((w, h))
+            if gray != 255 :
+                li.append(Char_Full)
+            else:
+                li.append(Char_Empty)
+        ret.append("".join(li))
+    dot = "\n".join(ret)
+    return  dot + '\n'
+
+def trim_dot(str):
+     ret = re.sub(Empty_Re, "\n", str)
+     return  ret
+
+def show_dot_pic(strs):
+    str = strs.strip(' ')
+    pic = create_image(str)
+    dot =  image_to_dot(pic, width=len(str) * SINGLE_WIDTH)
+    tri = trim_dot(dot)
+    print tri
+
+
+if __name__ == '__main__':
+    show_dot_pic('I LOVE YOU')
+
